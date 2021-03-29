ee7e3b37a87a4425af8eb61988d52ce70c943fee
Akvtsv <akvtsv@gmail.com>
2021-03-29 21:07:06 +0400

    Version 3 Allure ADD

--- src/test/java/MyGolosTest1.java
+++ src/test/java/MyGolosTest1.java
@@ -1,10 +1,15 @@
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.Test;
 
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
 
 public class MyGolosTest1 {
     @Test(timeout = 300000) //5 минут
-    public void userCanSearch() {
+     public void userCanSearch() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(false));
         open("https://google.com/ncr");
+        closeWebDriver();
     }
 }
