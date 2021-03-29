b524881ed5605b86608cca34ffd35aa7d6ec898c
Akvtsv <akvtsv@gmail.com>
2021-03-29 17:21:02 +0400

    Version 2

--- src/test/java/MyGolosTest1.java
+++ src/test/java/MyGolosTest1.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 import static com.codeborne.selenide.Selenide.open;
 
 public class MyGolosTest1 {
-    @Test(timeout = 1000)
+    @Test(timeout = 300000) //5 минут
     public void userCanSearch() {
         open("https://google.com/ncr");
     }
